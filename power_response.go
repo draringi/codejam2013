2fceadeff2d8e69d82ad77d3a2822eede79344c3
Michael Williams <draringi@draringi.net>
2013-11-16 06:05:04 -0500

    And now for the moment of truth...

---
+++ power_response.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"src/web"
+)
+
+func main() {
+	machine := new(web.MachineInterface)
+	http.HandleFunc("/", hello)
+	http.Handle("/upload", machine)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(os.Getenv("HOST")+":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "hello, world")
+}
