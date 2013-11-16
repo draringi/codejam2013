75243c00e8538d67ac9de7e8e07baf7ae023f96c
Michael Williams <draringi@draringi.net>
2013-11-16 15:21:40 -0500

    Added basic DB code

--- src/data/pulseParser.go
+++ src/data/pulseParser.go
@@ -7,15 +7,28 @@ import (
 	"strconv"
 	"time"
 //	"database/sql"
+	"os"
 )
 
 //import _ "github.com/jbarham/gopgsqldriver"
+
+const db_connection = "user=adminficeuc6 dbname=codejam2013 password=zUSfsRCcvNZf host="+os.Getenv("OPENSHIFT_POSTGRESQL_DB_HOST")+" port="+os.Getenv("OPENSHIFT_POSTGRESQL_DB_PORT")
+const db_provider = "postgres"
 
 const apikey = "B25ECB703CD25A1423DC2B1CF8E6F008"
 
 const day = "day"
 
 const quarter = (15*time.Minute)
+
+func db_init() {
+	var db, err = sql.Open(db_provider, db_connection)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS Records (ID SERIAL PRIMARY KEY UNIQUE,Time TIMESTAMP WITH TIME ZONE UNIQUE NOT NULL, Radiation DOUBLE precision, Humidity DOUBLE precision, Temperature DOUBLE precision, Wind DOUBLE precision, Power DOUBLE precision);
+}
 
 func getPast (id int, duration string) (resp *http.Response, err error) {
 	client := new(http.Client)
@@ -54,7 +67,25 @@ func parseXmlFloat64 (r io.Reader) []record {
 	return output.Records.RecordList
 }
 	
-/*
+func creativeUpdate(field string, data []record) {
+	var db, err = sql.Open(db_provider, db_connection)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	_, err = db.Exec("CREATE FUNCTION merge_db(key timestamp with time zone, data DOUBLE precision) RETURNS VOID AS $$ BEGIN LOOP UPDATE db SET $1 = data WHERE Time = key; IF found THEN RETURN; END IF; BEGIN INSERT INTO Records(Time,$1) VALUES (key, data); RETURN; EXCEPTION WHEN unique_violation THEN END; END LOOP; END; $$ LANGUAGE plpgsql;", field)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(data); i++ {
+		_, err = db.Exec("SELECT merge_db($1, $2);", data[i].Date, data[i].Value)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+
 func getPastDay () {
 	resp, err := getPast(66094, day) // Radiation
 	if err != nil {
@@ -70,7 +101,7 @@ func getPastDay () {
 	HumidityList := parseXmlFloat64(resp.Body)
 	resp.Body.Close()
 
-	resp, err = getPast(66077, day) // Temp
+	resp, err = getPast(66077, day) // Temperature
 	if err != nil {
 		panic(err)
 	}
@@ -90,5 +121,11 @@ func getPastDay () {
 	}
 	PowerList := parseXmlFloat64(resp.Body)
 	resp.Body.Close()
+
+	creativeUpdate("Radiation", RadList)
+	creativeUpdate("Humidity", HumidityList)
+	creativeUpdate("Temperature", TempList)
+	creativeUpdate("Wind", WindList)
+	creativeUpdate("Power", PowerList)
 }
-*/
+
