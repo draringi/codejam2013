93fea00223fdbea9ef38daa08d2d7e72eb17f130
Michael Williams <draringi@draringi.net>
2013-11-17 11:19:42 -0500

    JSON time

--- src/web/dashboard.go
+++ src/web/dashboard.go
@@ -4,16 +4,27 @@ import (
 	"net/http"
 	"draringi/codejam2013/src/forecasting"
 	"draringi/codejam2013/src/data"
+    "encoding/json"
+    "time"
 )
 
-type dashboardHelper struct {
+type future struct {
+    Records []record
+}
 
+type record struct {
+    Date time.Time
+    Power float64
+}
+
+type dashboardHelper struct {
+    Data *data.CSVData
+    Forcast future
 }
 
 type Dashboard struct {
 	channel chan (*data.CSVData)
 	JSONAid dashboardHelper
-	data *data.CSVData
 }
 
 func (self *Dashboard) Init () {
@@ -23,7 +34,7 @@ func (self *Dashboard) Init () {
 		for {
 			tmp := <-self.channel
             if tmp != nil {
-				self.data = tmp
+				self.JSONAid.Data = tmp
 			}
 		}
 	} ()
@@ -31,4 +42,23 @@ func (self *Dashboard) Init () {
 
 func (self *Dashboard) ServeHTTP (w http.ResponseWriter, request *http.Request) {
 	http.ServeFile(w, request, "dashboard.html")
+}
+
+func (self *dashboardHelper) Build (Data *data.CSVData) {
+    self.Data = Data
+    self.Forcast = new(future)
+    self.Forcast.Records = make([]record,len(Data.Data))
+    for i :=0; i<len(Data.Data); i++ {
+        self.Forcasts.Records[i].Date = Data.Data[i].Time
+        self.Forcasts.Records[i].Power = Data.Data[i].Power
+    }
+}
+
+func (self *dashboardHelper) jsonify (w http.ResponseWriter) {
+    encoder := json.NewEncoder(w)
+    encoder.encode(self.Forcast)
+}
+
+func (self *dashboardHelper) ServeHTTP (w http.ResponseWriter, request *http.Request) {
+    self.jsonify(w)
 }
