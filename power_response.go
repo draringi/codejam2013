9b0ac541dc7cc36bf9562517cecdcc94f68bf596
Michael Williams <draringi@draringi.net>
2013-11-17 09:55:22 -0500

    Started work on static html and js environment

--- power_response.go
+++ power_response.go
@@ -12,6 +12,7 @@ func main() {
 	dashboard := new(web.Dashboard)
 	http.Handle("/upload", machine)
 	http.Handle("/", dashboard)
+    http.Handle("/static", http.FileServer(http.Dir("static")))
 	fmt.Println("listening...")
 	err := http.ListenAndServe(os.Getenv("HOST")+":"+os.Getenv("PORT"), nil)
 	if err != nil {
