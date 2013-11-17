ed54ce15206563f872fbfa343a9dee3d73665cf9
Michael Williams <draringi@draringi.net>
2013-11-17 16:57:23 -0500

    js fixes

--- static/js/predictions.js
+++ static/js/predictions.js
@@ -1,16 +1,14 @@
 function updateChart (data) {
 	var list = [];
 	var records = data.Records;
-	console.log(records);
 	var len = records.length;
 	for (var i = 0; i < len; i++) {
 		list.push([records[i].Date, records[i].Power])
 	}
-	console.log(list);
 	$.jqplot('futureplot', [list], {
 		title:'Predicted Power Usage for the next 24 Hours',
 		axes:{xaxis:{renderer:$.jqplot.DateAxisRenderer}},
-		series:[{lineWidth:4, markerOptions:{style:'square'}}]
+		series:[{lineWidth:4, markerOptions:{show:false}}]
 	});
 }
 
